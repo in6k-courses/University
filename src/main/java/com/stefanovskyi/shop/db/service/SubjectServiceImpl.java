1ca2367298ce3c709dff8978aa51f40e2c88dfb4
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 18:23:56 +0200

    Implement Subject service

---
+++ src/main/java/com/stefanovskyi/shop/db/service/SubjectServiceImpl.java
@@ -0,0 +1,32 @@
+package com.stefanovskyi.shop.db.service;
+
+import com.stefanovskyi.shop.db.SubjectRepository;
+import com.stefanovskyi.shop.model.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class SubjectServiceImpl implements SubjectService {
+
+    @Autowired
+    SubjectRepository subjectRepository;
+
+    @Override
+    public List<Subject> getAll() {
+        return subjectRepository.findAll();
+    }
+
+    @Override
+    public Subject getOne(Integer id) {
+        return subjectRepository.getOne(id);
+    }
+
+    @Override
+    public void add(Subject subject) {
+        subjectRepository.save(subject);
+    }
+}
