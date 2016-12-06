74e027c4707e29568e9b913d0a0410e0de8a7983
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 17:09:40 +0200

    Implement get one student method

--- src/main/java/com/stefanovskyi/shop/controller/StudentController.java
+++ src/main/java/com/stefanovskyi/shop/controller/StudentController.java
@@ -1,16 +1,31 @@
 package com.stefanovskyi.shop.controller;
 
+import com.stefanovskyi.shop.db.service.StudentService;
+import com.stefanovskyi.shop.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
+    @Autowired
+    StudentService studentService;
+
     @RequestMapping("/")
     @ResponseBody
-    private String getAllStudents() {
-        return "Test all students";
+    private List<Student> getAllStudents() {
+        return studentService.getAll();
+    }
+
+    @RequestMapping("/{id}")
+    @ResponseBody
+    private Student getTask(@PathVariable("id") Integer id) {
+        return studentService.getOne(id);
     }
 }
