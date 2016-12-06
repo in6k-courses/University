814caf4301557861fd12ebd036993e102169c70e
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 13:53:13 +0200

    Implement subject entity

---
+++ src/main/java/com/stefanovskyi/shop/model/Subject.java
@@ -0,0 +1,55 @@
+package com.stefanovskyi.shop.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subject")
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @Column(name = "studentId")
+    private Student studentId;
+
+    @ManyToOne
+    @Column(name = "teacherId")
+    private Teacher teacherId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Student studentId) {
+        this.studentId = studentId;
+    }
+
+    public Teacher getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Teacher teacherId) {
+        this.teacherId = teacherId;
+    }
+}
