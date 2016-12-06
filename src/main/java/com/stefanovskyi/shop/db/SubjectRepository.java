c63c101d97d44e0423638f710a578dcafc505d6f
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 18:14:57 +0200

    Add Subject repository

---
+++ src/main/java/com/stefanovskyi/shop/db/SubjectRepository.java
@@ -0,0 +1,9 @@
+package com.stefanovskyi.shop.db;
+
+import com.stefanovskyi.shop.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+}
