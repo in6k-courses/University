75a6b8af51dac41e62e19abd174b1c277066ef2a
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 12:49:21 +0200

    Add hibernate, add entity

--- src/main/java/com/stefanovskyi/shop/model/Item.java
+++ src/main/java/com/stefanovskyi/shop/model/Item.java
@@ -1,7 +1,45 @@
 package com.stefanovskyi.shop.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
     private Integer id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "price")
     private Integer price;
+
+    public Item() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
