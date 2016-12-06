550105e9750466840909198041a4d01c4ad1c00f
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 14:06:02 +0200

    Implement student service

---
+++ src/main/java/com/stefanovskyi/shop/db/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.stefanovskyi.shop.db.service;
+
+import com.stefanovskyi.shop.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAll();
+    Student getOne();
+    void add(Student student);
+    void delete(Student student);
+    void update(Student student);
+}
