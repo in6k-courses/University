afa48570a5faed53e0167dfbe22f3190db2c4ca3
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 18:21:47 +0200

    Implement subject controller

--- src/main/java/com/stefanovskyi/shop/controller/SubjectController.java
+++ src/main/java/com/stefanovskyi/shop/controller/SubjectController.java
@@ -1,15 +1,31 @@
 package com.stefanovskyi.shop.controller;
 
+import com.stefanovskyi.shop.db.service.SubjectService;
+import com.stefanovskyi.shop.model.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/subject")
 public class SubjectController {
+
+    @Autowired
+    SubjectService subjectService;
+
     @RequestMapping("/")
     @ResponseBody
-    private String getAllSubjects() {
-        return "Test all subjects";
+    private List<Subject> getAllSubjects() {
+        return subjectService.getAll();
+    }
+
+    @RequestMapping("/{id}")
+    @ResponseBody
+    private Subject getTask(@PathVariable("id") Integer id) {
+        return subjectService.getOne(id);
     }
 }
