dec1cfc88e296783adb41a41829d5e6606e048fb
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 13:53:30 +0200

    Implement teacher entity

---
+++ src/main/java/com/stefanovskyi/shop/model/Teacher.java
@@ -0,0 +1,46 @@
+package com.stefanovskyi.shop.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "teacher")
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany
+    @Column(name = "subId")
+    private Subject subId;
+
+    public Teacher() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Subject getSubId() {
+        return subId;
+    }
+
+    public void setSubId(Subject subId) {
+        this.subId = subId;
+    }
+}
