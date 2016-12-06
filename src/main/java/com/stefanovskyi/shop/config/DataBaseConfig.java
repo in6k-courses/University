9e42b8ff1e1bb78a8bf80680479cba28fd9d6693
astefanovskiy <astefanovskiy@gmail.com>
2016-12-06 22:29:10 +0200

    Remove useless methods

--- src/main/java/com/stefanovskyi/shop/config/DataBaseConfig.java
+++ src/main/java/com/stefanovskyi/shop/config/DataBaseConfig.java
@@ -1,13 +1,10 @@
 package com.stefanovskyi.shop.config;
 
 import com.stefanovskyi.shop.PropertyHandler;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -36,23 +33,6 @@ public class DataBaseConfig {
         properties.put("hibernate.show_sql", getPropertyHandler().getShowSql());
         properties.put("hibernate.hbm2ddl.auto", getPropertyHandler().getHbm2ddlAuto());
         return properties;
-    }
-
-    @Bean
-    public LocalSessionFactoryBean getSessionFactoryBean(DataSource dataSource) throws URISyntaxException {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource);
-        String currentPackage = "/com/stefanovskyi/shop";
-        sessionFactoryBean.setPackagesToScan(currentPackage);
-        sessionFactoryBean.setHibernateProperties(this.getHibernateProperties());
-        return sessionFactoryBean;
-    }
-
-    @Bean
-    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory);
-        return transactionManager;
     }
 
     @Bean
