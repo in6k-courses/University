e2d4bcb1636781f4d1865e3bb1c40aec8e6810d2
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 18:16:33 +0200

    Add spring MVC annotation

--- src/main/java/com/stefanovskyi/shop/DemoApplication.java
+++ src/main/java/com/stefanovskyi/shop/DemoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @EnableAutoConfiguration
 @EnableJpaRepositories("com.stefanovskyi.shop")
 public class DemoApplication {
