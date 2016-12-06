7e679e18c9fc3cd9afc9cd38267976f7b9b45363
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-06 18:03:52 +0200

    Add appropriate annotations

--- src/main/java/com/stefanovskyi/shop/DemoApplication.java
+++ src/main/java/com/stefanovskyi/shop/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableJpaRepositories("com.stefanovskyi.shop.db")
+@EnableJpaRepositories("com.stefanovskyi.shop")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/stefanovskyi/shop/db/service/StudentServiceImpl.java
+++ src/main/java/com/stefanovskyi/shop/db/service/StudentServiceImpl.java
@@ -3,9 +3,13 @@ package com.stefanovskyi.shop.db.service;
 import com.stefanovskyi.shop.db.StudentRepository;
 import com.stefanovskyi.shop.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class StudentServiceImpl implements StudentService {
 
     @Autowired
