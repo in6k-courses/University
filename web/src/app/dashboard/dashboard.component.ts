69cfa5bec25535b954fcfc22567dc5b610c80725
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-13 11:33:34 +0200

    Implement button to show/hide adding-form

--- web/src/app/dashboard/dashboard.component.ts
+++ web/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Teacher } from "../model/teacher";
 import { TeacherService } from "../services/teacher.service";
+import {sha1} from "@angular/compiler/src/i18n/digest";
 
 @Component({
   moduleId: module.id.toString(),
@@ -9,13 +10,21 @@ import { TeacherService } from "../services/teacher.service";
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  show:boolean;
+  addButtonText:string;
   teachers: Teacher [] = [];
   constructor(private teacherService : TeacherService) {}
 
   ngOnInit() {
+    this.show = true;
+    this.addButtonText = "Add student";
     this.teacherService.getTeachers()
       .then(teachers => this.teachers = teachers.slice(1,5));
   }
 
+  showAddForm(): void {
+      this.show = !this.show;
+      if (this.show) this.addButtonText = "Add student";
+      else this.addButtonText = "Hide form";
+  }
 }
