f5812cb3867223bb46eae71e8a738d15bad1e9b6
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-13 17:59:17 +0200

    Remove useless libraries

--- web/src/app/services/student.service.ts
+++ web/src/app/services/student.service.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { Http, Headers } from "@angular/http";
 import { Student } from '../model/student'
 
 @Injectable()
