5765ace762dc338820ee0530387f26ef8a2859a4
Oleksandr Stefanovskyi <oleksandrs@softwareplanet.uk.com>
2016-12-15 00:57:40 +0200

    Refactoring: Clean code of useless providers, remove useless component

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { StudentComponent } from "./student/student.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { StudentDetailComponent } from "./student-detail/student-detail.component";
 import { SubjectComponent } from "./subject/subject.component";
 import {TeacherComponent} from "./teacher/teacher.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
-  { path: 'detail/:id', component: StudentDetailComponent },
   { path: 'students',   component: StudentComponent },
   { path: 'subjects',   component: SubjectComponent },
   { path: 'teachers',   component: TeacherComponent }
--- web/src/app/subject/subject.component.ts
+++ web/src/app/subject/subject.component.ts
@@ -1,20 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {SubjectService} from "./subject.service";
 import {Subject} from "../model/subject";
-import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-subject',
     templateUrl: './subject.component.html',
-    styleUrls: ['./subject.component.css'],
-    providers: [SubjectService]
+    styleUrls: ['./subject.component.css']
 })
 export class SubjectComponent implements OnInit {
     subjects: Subject[];
     selectedSubject: Subject;
-    constructor(
-        private router: Router,
-        private studentService: SubjectService) { }
+    constructor(private studentService: SubjectService) { }
 
     getSubjects(): void {
         this.studentService
